249280efaa53ceb14bd450f5a072db401e49a189
Fernando Mumbach <fermuch@ayvu.net>
2026-04-04 23:43:24 -0300

    bump version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Sscmex.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @github_url "https://github.com/monoflow-ayvu/sscmex"
   @mix_target_triplets %{
     "nerves_system_sg2002" => {"riscv64", "unknown", "linux-musl"}
